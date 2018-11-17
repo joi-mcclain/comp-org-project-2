013297e4d1e6b6c969d95834fb09f9a1b5a3a38b
joi-mcclain <44388650+joi-mcclain@users.noreply.github.com>
2018-11-16 19:21:47 -0500

    buffer size and error text
    set buffer size for user input to 10000, set buffer size for spaceless user input to 4, and initialize error text for empty, long, and invalid input

--- project.s
+++ project.s
@@ -1,1 +1,6 @@
 .data
+  userInput:      .space  10000
+  emptyInput:     .asciiz "Input is empty."
+  longInput:      .asciiz "Input is too long"
+  invalidInput:   .asciiz "Invalid base-33 number."
+  numericInput:   .space 4
